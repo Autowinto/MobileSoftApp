2ddd9c5dbb4f48db92e2baa861bcc6348ebe731d
Magnus Christian Larsen <magn3323@gmail.com>
2023-10-14 16:11:37 +0200

    changed initial name

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="CarList">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen
             name="Login"
             component={Login}
