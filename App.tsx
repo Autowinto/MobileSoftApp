e66e629c2f5bccae96b50c873dc2e5c6d42b73dc
Magnus Christian Larsen <magn3323@gmail.com>
2023-10-14 13:29:10 +0200

    initialized repo with proper template

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Default Text</Text>
-
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
