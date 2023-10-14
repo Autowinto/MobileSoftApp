e1d6eb7f1455fb9af698ec4efc10ac99977e11d9
Magnus Christian Larsen <magn3323@gmail.com>
2023-10-14 14:02:41 +0200

    added basic content to login page

--- views/Login.tsx
+++ views/Login.tsx
@@ -3,8 +3,20 @@ import { View, Text, StyleSheet, Button } from "react-native"
 export default function Login({ navigation }) {
   return (
     <View style={styles.container}>
+      <Text>Autorent</Text>
+      <Text>... din foretrukne biludlejningsekspert</Text>
       <Text>Login Page</Text>
-      <Button title="Press" onPress={() => navigation.navigate("Details")} />
+      <Button
+        title="Opret en bruger med Apple"
+        onPress={() => navigation.navigate("Details")}
+      />
+      <Button
+        title="Andre oprettelsesmuligheder"
+        onPress={() => navigation.navigate("Details")}
+      />
+
+      <Text>Har du allerede en bruger?</Text>
+      <a>Login her</a>
     </View>
   )
 }
